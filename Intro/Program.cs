d034910ea880eed3d8652dda087cb65de09db979
Jacob McAllister <mcallisterj@westerntc.edu>
2025-03-03 13:50:42 -0600

    End of class
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

---
+++ Intro/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = new int[8];
+
+int[] numbersAgain = {3, 4, 6};
+
+//==================
+
+
+List<int> numbersProper = 
+    new List<int>{3, 4, 6};
+
+foreach(int elem in numbersProper){
+    Console.WriteLine($"elem: {elem}");
+}
