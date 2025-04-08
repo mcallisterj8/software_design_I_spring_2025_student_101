aab3502bd799ca1bdd6b45c0da698863af547765
Jacob McAllister <datadeltaanalysis@protonmail.com>
2025-04-08 12:09:45 -0500

    Class Prep
    Signed-off-by: Jacob McAllister <datadeltaanalysis@protonmail.com>

---
+++ GenericMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿iPhone iphoneA = new iPhone { Name = "apple" };
+iPhone iphoneB = new iPhone { Name = "berry" };
+iPhone iphoneC = new iPhone { Name = "cherry" };
+iPhone iphoneD = new iPhone { Name = "kiwi" };
+
+Android androidA = new Android { Name = "date" };
+Android androidB = new Android { Name = "elderberry" };
+Android androidC = new Android { Name = "kiwi" };
+
+Toolbox toolbox = new Toolbox { Name = "ToolboxA" };
